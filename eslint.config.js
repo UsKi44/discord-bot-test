6578bab28b91955015ad31c62d989f22346764d3
UsKi44 <lpirtskhalava2001@gmail.com>
2025-01-10 01:49:40 +0400

    minor changes

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+const js = require("@eslint/js");
+
+module.exports = [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+    },
+    rules: {
+      "brace-style": ["error", "stroustrup", { allowSingleLine: true }],
+      "comma-dangle": ["error", "always-multiline"],
+      "no-console": "off",
+      "no-empty-function": "error",
+      "no-shadow": ["error", { allow: ["err", "resolve", "reject"] }],
+      "no-var": "error",
+      "no-undef": "off",
+      "prefer-const": "error",
+      quotes: ["error", "single"],
+      semi: ["error", "always"],
+    },
+  },
+];
