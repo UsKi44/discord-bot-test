1a04b03cd5ef45664de6ae63c26a5502dc3c60b1
Luka <l.phirtskhalava@solvio.dev>
2025-01-12 03:19:06 +0400

    added handling/deploying commands & removed eslint for now

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,21 @@
-const js = require("@eslint/js");
+// const js = require("@eslint/js");
 
-module.exports = [
-  js.configs.recommended,
-  {
-    languageOptions: {
-      ecmaVersion: "latest",
-    },
-    rules: {
-      "brace-style": ["error", "stroustrup", { allowSingleLine: true }],
-      "comma-dangle": ["error", "always-multiline"],
-      "no-console": "off",
-      "no-empty-function": "error",
-      "no-shadow": ["error", { allow: ["err", "resolve", "reject"] }],
-      "no-var": "error",
-      "no-undef": "off",
-      "prefer-const": "error",
-      quotes: ["error", "single"],
-      semi: ["error", "always"],
-    },
-  },
-];
+// module.exports = [
+//   js.configs.recommended,
+//   {
+//     languageOptions: {
+//       ecmaVersion: "latest",
+//     },
+//     rules: {
+//       "brace-style": ["error", "stroustrup", { allowSingleLine: true }],
+//       "comma-dangle": ["error", "always-multiline"],
+//       "no-console": "off",
+//       "no-empty-function": "error",
+//       "no-shadow": ["error", { allow: ["err", "resolve", "reject"] }],
+//       "no-var": "error",
+//       "no-undef": "off",
+//       "prefer-const": "error",
+//       semi: ["error", "always"],
+//     },
+//   },
+// ];
