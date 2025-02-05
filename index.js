82c495e04f2130bb091e9c21b04327cb89d892b2
UsKi44 <lpirtskhalava2001@gmail.com>
2025-02-05 20:11:10 +0400

    .

--- index.js
+++ index.js
@@ -139,4 +139,7 @@ const server = http.createServer((req, res) => {
   res.end("Bot is running!");
 });
 
-server.listen(process.env.PORT || 3000);
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
